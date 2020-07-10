418fc60729e5c6a72d6aca809ae0d986442e9842
Amy Simpson <simpson.r.amy@gmail.com>
2020-07-10 23:24:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function breakOut(array, changeValue, stopValue){
 	for (let i = 0; i < array.length; i++) {
 	  if (array[i] !== stopValue) {
 	    array[i] = changeValue;
-	  } else{
+	  } else {
 	  break;
 	  }
 	}
